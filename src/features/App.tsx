6d1ecbee95d2d25261d455a4b228eb4c1a5eea13
naoki-funawatari <g011c1180@gmail.com>
2022-02-19 19:06:59 +0900

    feat: RecoilRoot の位置を移動。

--- src/features/App.tsx
+++ src/features/App.tsx
@@ -1,13 +1,12 @@
-import { RecoilRoot } from "recoil";
 import Header from "@/features/Header";
 import Main from "@/features/Main";
 
 export default function App() {
   return (
-    <RecoilRoot>
+    <>
       <Header />
       <hr />
       <Main />
-    </RecoilRoot>
+    </>
   );
 }
