76a27fe9ff5822fb51586fc2eb46e4facbf9b41a
naoki-funawatari <g011c1180@gmail.com>
2022-02-23 19:31:22 +0900

    feat: scss 導入。

--- src/features/App.tsx
+++ src/features/App.tsx
@@ -1,5 +1,6 @@
 import Selection from "@/features/selection/Selection";
 import Details from "@/features/details/Details";
+import "@/assets/styles.scss";
 
 export default function App() {
   return (
