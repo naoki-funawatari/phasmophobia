a0db4366c96fb1a39cb6c3ff509b67bc3c99ab8b
naoki-funawatari <g011c1180@gmail.com>
2022-02-27 01:09:21 +0900

    feat: Suspense でラップ。

--- src/features/App.tsx
+++ src/features/App.tsx
@@ -1,13 +1,14 @@
+import { Suspense } from "react";
 import Selection from "@/features/selection/Selection";
 import Details from "@/features/details/Details";
 import "@/assets/styles.scss";
 
 export default function App() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Selection />
       <br />
       <Details />
-    </>
+    </Suspense>
   );
 }
