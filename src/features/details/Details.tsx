92ac6966f8ce91f9a20e3a91d311876296ad5dff
naoki-funawatari <g011c1180@gmail.com>
2022-03-11 22:18:32 +0900

    feat: スクロールバーを導入。

--- src/features/details/Details.tsx
+++ src/features/details/Details.tsx
@@ -1,3 +1,4 @@
+import { Scrollbars } from "react-custom-scrollbars";
 import { useGhosts, useItemConditions } from "@/features/common/hooks";
 import Detail from "@/features/details/Detail";
 
@@ -31,31 +32,33 @@ export default function Details() {
 
   return (
     <main>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>
-              <div className="determin-name">証拠①</div>
-            </th>
-            <th>
-              <div className="determin-name">証拠②</div>
-            </th>
-            <th>
-              <div className="determin-name">証拠③</div>
-            </th>
-            <th>カウント</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sortedGhosts.map((ghost, index) => (
-            <Detail
-              key={`ghost-table-data-${ghost.id}-${index}`}
-              {...{ ghost, index }}
-            />
-          ))}
-        </tbody>
-      </table>
+      <Scrollbars height="100%" universal>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>
+                <div className="determin-name">証拠①</div>
+              </th>
+              <th>
+                <div className="determin-name">証拠②</div>
+              </th>
+              <th>
+                <div className="determin-name">証拠③</div>
+              </th>
+              <th>カウント</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedGhosts.map((ghost, index) => (
+              <Detail
+                key={`ghost-table-data-${ghost.id}-${index}`}
+                {...{ ghost, index }}
+              />
+            ))}
+          </tbody>
+        </table>
+      </Scrollbars>
     </main>
   );
 }
